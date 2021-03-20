7308837105096df7011014874918dbfacf624742
someone-here <eshgupta1712@gmail.com>
2021-03-20 16:34:49 +0530

    commit

--- Dank_bot2.py
+++ Dank_bot2.py
@@ -7,8 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 em = input("Enter discord email: ")
 pas = input("Enter discord password: ")
-commands = ["Pls beg", "Pls postmeme", "k", "pls fish", "pls hunt", "pls deposit max"]
-url = input("Enter the channel url")
+commands = ["Pls beg", "Pls postmeme", "k",
+            "pls fish", "pls hunt", "pls deposit max"]
+url = input("Enter the channel url: ")
 
 driver = webdriver.Chrome()
 driver.get(url)
@@ -20,13 +21,14 @@ password.send_keys(pas)
 password.send_keys(Keys.RETURN)
 try:
     element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div/div/div/div/div[3]/div[2]'))
+        EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div/div/div/div/div[3]/div[2]'))
     )
 except:
     driver.quit()
-    
+
 while True:
     for i in commands:
-            element.send_keys(i + "\n")
-            sleep(1)
+        element.send_keys(i + "\n")
+        sleep(1)
     sleep(60)
