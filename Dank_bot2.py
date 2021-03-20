f8a5aae459e70bca41ce6d60aa4de1ca5f8a7e3e
someone-here <eshgupta1712@gmail.com>
2021-03-20 17:11:15 +0530

    commit

--- Dank_bot2.py
+++ Dank_bot2.py
@@ -5,13 +5,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+
 em = input("Enter discord email: ")
 pas = input("Enter discord password: ")
 commands = ["Pls beg", "Pls postmeme", "k",
             "pls fish", "pls hunt", "pls deposit max"]
 url = input("Enter the channel url: ")
 
-driver = webdriver.Chrome()
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(chrome_options=options)
 driver.get(url)
 
 email = driver.find_element_by_name("email")
@@ -22,7 +25,7 @@ password.send_keys(Keys.RETURN)
 try:
     element = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located(
-            (By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div/div/div/div/div[3]/div[2]'))
+            (By.CSS_SELECTOR, '#app-mount > div.app-1q1i1E > div > div.layers-3iHuyZ.layers-3q14ss > div > div > div > div > div.chat-3bRxxu > div.content-yTz4x3 > main > form > div > div > div > div > div > div.textArea-12jD-V.textAreaSlate-1ZzRVj.slateContainer-3Qkn2x > div.markup-2BOw-j.slateTextArea-1Mkdgw.fontSize16Padding-3Wk7zP'))
     )
 except:
     driver.quit()
